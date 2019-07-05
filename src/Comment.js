ea2731eb9f521eb4cf00eb17941519e9b5ea6de4
Roysan Easo <easo.roysan@gmail.com>
2019-07-05 11:18:20 -0500

    everything done with delete button. will confirm comment deletion before removing li

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,65 @@
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+let imageId = 2923
+
 class Comment {
 
-    constructor(content){
+    constructor(comment, id=null){
+        //makes comment object something that can be called after class instance is made
+        this.comment = comment
+
+        //makes id referencable if it was passed
+        if(id){
+            this.id = id
+        }
+
+        //makes li for comment
         this.li = document.createElement('li')
-        this.li.innerText = content
+        this.li.innerText = this.comment
+
+        //makes del button and adds it to the li
+        this.del = document.createElement('button')
+        this.del.innerText = 'Delete'
+        this.li.append(this.del)
+
+        this.del.addEventListener('click', () =>{
+            this.delete()
+        })
+
     }
 
     render(){
         return this.li
     }
+
+    post(){
+        //put comment in the database
+        fetch(commentsURL,{
+            method: 'POST',
+            headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+            image_id: imageId,
+            content: this.comment
+            })
+        }).then(response => response.json())
+        //sets id to use for deleting
+        .then(result => this.id = result.id)
+    }
+
+    delete(){
+
+        //deletes comment from backend
+        fetch(commentsURL + this.id,{
+            method: 'DELETE'
+        }).then(response => response.json())
+        //confirms that comment was deleted
+        .then(result => this.li.innerText=result.message)
+        //after 1 second, removes the comment line
+        .then(setTimeout( ( () => {
+            this.li.remove()
+        }),1000))
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -7,23 +7,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  //get image and image's comments
   fetch(imageURL)
     .then(response => response.json())
     .then(result => {
+
+      //makes image instance and puts it on page
       myImage = new Image(result.url,result.name)
       document.querySelector('#image_container').append(myImage.render())
+
+      //puts each comment on page in li
       result.comments.forEach( comment => {
-        newComment = new Comment(comment.content)
+        newComment = new Comment(comment.content,comment.id)
         document.querySelector('#comments').append(newComment.render())
       })
     })
 
+  //gets comment form to get submitted info
   commentBox = document.querySelector('#comment_form')
+
+  //gets submit button and adds event listener
   submitButton = document.querySelector("#submit_button")
   submitButton.addEventListener('click', (e)=>{
+    //stops page from refreshing
     e.preventDefault()
 
+    //create comment, render it to the page, and post it to the backend
     newComment = new Comment(commentBox.children[0].value)
     document.querySelector('#comments').append(newComment.render())
+    newComment.post()
+
+    //clears out comment input
+    commentBox.children[0].value = null
+
   })
+
+
 })
